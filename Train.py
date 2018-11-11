be07ec6ec160759a95f810b90b9201cc2d9e0096
falvad01 <falvad01@estudiantes.unileon.es>
2018-11-11 18:16:27 +0100

    Entrenamiento casi terminado

--- Train.py
+++ Train.py
@@ -10,5 +10,132 @@ from tensorflow.python.keras import backend as K #Si hay una sesion de keras la 
 
 K.clear_session() #Matamos la sescion de keras anterior
 
+trainingData = './Entrenamiento' #Guardamos el directorio de las imagenes
+validationData = './Validacion'
 
 
+gen = 20 #generaciones
+alt = 100 # tamanio imagenes
+lon = 100
+bachSize = 32 
+steps = 1000 # numero de veces que se procesa la imagen por gen
+validationSteps = 200
+conv1Filters = 32 #profundidad de la imagen en cada convolucion
+conv2Filters = 64
+filter1Size =  (3,3) #tamanio de los filtros 
+filter2Size = (2,2)
+poolSize = (2,2)
+clas = 3 #curvasD, curvasI, rectas
+learnigRate = 0.0005 #ajustes de la red neuronal para ajuste optimo
+
+
+#PREPROCESAMIENTO DE LAS IMAGENES
+
+dataGenTraining = ImageDataGenerator(
+    rescale = 1./255, #los valores de los pixeles se reducen a 0 1
+    shear_range = 0.3, #enseñamos al algortimo que las imagenes pueden estar inclinadas
+    zoom_range = 0.3, #enseñamos al algoritmo que algunas imaenes estan mas cerca que otras
+    horizontal_flip = True #invertimos las imagenes para enseñar al aglorimo direccionalidad
+)
+
+dataGenValidation = ImageDataGenerator(
+    rescale = 1./255 #para validar las imagenes tal cual son, sin girar ni zoom ni nada
+)
+
+trainingImage = dataGenTraining.flow_from_directory(#transformamos las imagenes
+    trainingData,
+    target_size = (alt,lon),
+    bach_size = bachSize,
+    class_mode = 'Categotical' #clasifacion categorica de cada imgen, recta curbad y cubai
+
+)
+
+validationImage = dataGenValidation.flow_from_director(
+    validationData,
+    target_size = (alt,lon),
+    bach_size = bachSize,
+    class_mode = 'Categotical'
+)
+
+#CREAMOS LAS RED CNN
+
+cnn = Sequential() #La red son varias capas apiladas
+
+cnn.add(#creamos la primera capa
+    Convolution2D(  #sera una convolucion
+        conv1Filters,  #32 filtros
+        filter1Size, # de tamanio 3,3
+        padding = 'same',
+        imput_shape = (alt,lon),  #las imagenes de entrada son de este tamanio
+        activation = 'relu' 
+    )
+)
+
+cnn.add( #creamos una segunda capa
+    MaxPooling2D( # de tipo maxPooling
+        pool_size = poolSize  #y este es su tamanio 
+    )
+)
+
+cnn.add( #tercera capa
+    Convolution2D(
+        conv2Filters,
+        filter2Size,
+        padding = 'same',
+        activation = 'relu' 
+    )
+)
+
+cnn.add( #cuarta capa
+    MaxPooling2D(
+        pool_size = poolSize
+    )
+)
+
+cnn.add(
+    Flatten()#la imagen que es muy profunda despues de pasar por los filtros la aplanamos para concentrar toda la informacion
+)
+
+cnn.add( #capa normal(5)
+    Dense(
+        256, #numero de nuronas
+        activation = 'relu'
+    )
+)
+
+cnn.add(
+    Dropout(0,5) #durante el entrenamiento apagamos la mitad de las neuronas,esto nos permite evitar el sobreajuste
+)
+
+cnn.add(#ultima capa(6)
+    Dense(
+        clas,
+        activation = 'softmax' #nos da la provavilidad de que sea una cosa u otra
+    )
+)
+
+cnn.compile(
+    loss = 'categorical_crossentropy', #indica si va bien o mal
+    optimizer = optimizers.Adam(
+        lr = learnigRate #optimizador 
+    ),
+    metrics = ['acuracy']
+)
+
+cnn.fit( #con los que vamos a entrenar la imagen
+    trainingImage,
+    steps_per_epoch = steps,
+    epochs = gen,
+    imagen_validacion = validationImage,
+    validation_steps = validationSteps
+)
+
+#GUARDAMOS EL MODELO EN UN ARCHIVO
+
+dir = './model'
+
+if not os.path.exists(dir)
+    os.mkdir(dir)
+
+cnn.save('./model/model.h') #guardamos la estructura del modelo
+cnn.save_weights('./model/model.h') #gaurdamos los pesos de cada capa
